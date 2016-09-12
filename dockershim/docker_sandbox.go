kubernetes/kubernetes

dockershim: utilize the Metadata in container names
This commit changes how the shim constructs and parses docker container names
by using the new "Metadata" types.

pkg/kubelet/dockershim/docker_sandbox.go
@@ -22,6 +22,7 @@ import (
 	dockertypes "github.com/docker/engine-api/types"
 	dockercontainer "github.com/docker/engine-api/types/container"
 	dockerfilters "github.com/docker/engine-api/types/filters"
+	"github.com/golang/glog"
 
 	runtimeApi "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 )
@@ -117,7 +118,7 @@ func (ds *dockerService) PodSandboxStatus(podSandboxID string) (*runtimeApi.PodS
 	network := &runtimeApi.PodSandboxNetworkStatus{Ip: &IP}
 	netNS := getNetworkNamespace(r)
 
-	podName, podNamespace, podUID, attempt, err := parseSandboxName(r.Name)
+	metadata, err := parseSandboxName(r.Name)
 	if err != nil {
 		return nil, err
 	}
@@ -126,12 +127,7 @@ func (ds *dockerService) PodSandboxStatus(podSandboxID string) (*runtimeApi.PodS
 		Id:        &r.ID,
 		State:     &state,
 		CreatedAt: &ct,
-		Metadata: &runtimeApi.PodSandboxMetadata{
-			Name:      &podName,
-			Namespace: &podNamespace,
-			Uid:       &podUID,
-			Attempt:   &attempt,
-		},
+		Metadata:  metadata,
 		// TODO: We write annotations as labels on the docker containers. All
 		// these annotations will be read back as labels. Need to fix this.
 		// Also filter out labels only relevant to this shim.
@@ -184,18 +180,22 @@ func (ds *dockerService) ListPodSandbox(filter *runtimeApi.PodSandboxFilter) ([]
 	result := []*runtimeApi.PodSandbox{}
 	for i := range containers {
 		c := containers[i]
-		if len(filter.GetName()) > 0 {
-			sandboxName, _, _, _, err := parseSandboxName(c.Names[0])
-			if err != nil || sandboxName != filter.GetName() {
-				continue
-			}
-		}
 
-		s := toRuntimeAPISandbox(&c)
-		if filterOutReadySandboxes && s.GetState() == runtimeApi.PodSandBoxState_READY {
+		converted, err := toRuntimeAPISandbox(&c)
+		if err != nil {
+			glog.V(5).Infof("Unable to convert docker to runtime API container: %v", err)
 			continue
 		}
-		result = append(result, s)
+		if len(filter.GetName()) > 0 && converted.Metadata.GetName() != filter.GetName() {
+			// TODO: Remove "name" from the SandboxFilter because name can no
+			// longer be used to identify a container.
+			continue
+		}
+		if filterOutReadySandboxes && converted.GetState() == runtimeApi.PodSandBoxState_READY {
+			continue
+		}
+
+		result = append(result, converted)
 	}
 	return result, nil
 }
@@ -208,7 +208,7 @@ func makeSandboxDockerConfig(c *runtimeApi.PodSandboxConfig, image string) *dock
 
 	hc := &dockercontainer.HostConfig{}
 	createConfig := &dockertypes.ContainerCreateConfig{
-		Name: buildSandboxName(c),
+		Name: makeSandboxName(c),
 		Config: &dockercontainer.Config{
 			Hostname: c.GetHostname(),
 			// TODO: Handle environment variables.

Mirantis/cri-dockerd

dockershim/docker_sandbox.go
@@ -22,6 +22,7 @@ import (
 	dockertypes "github.com/docker/engine-api/types"
 	dockercontainer "github.com/docker/engine-api/types/container"
 	dockerfilters "github.com/docker/engine-api/types/filters"
+	"github.com/golang/glog"
 
 	runtimeApi "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 )
@@ -117,7 +118,7 @@ func (ds *dockerService) PodSandboxStatus(podSandboxID string) (*runtimeApi.PodS
 	network := &runtimeApi.PodSandboxNetworkStatus{Ip: &IP}
 	netNS := getNetworkNamespace(r)
 
-	podName, podNamespace, podUID, attempt, err := parseSandboxName(r.Name)
+	metadata, err := parseSandboxName(r.Name)
 	if err != nil {
 		return nil, err
 	}
@@ -126,12 +127,7 @@ func (ds *dockerService) PodSandboxStatus(podSandboxID string) (*runtimeApi.PodS
 		Id:        &r.ID,
 		State:     &state,
 		CreatedAt: &ct,
-		Metadata: &runtimeApi.PodSandboxMetadata{
-			Name:      &podName,
-			Namespace: &podNamespace,
-			Uid:       &podUID,
-			Attempt:   &attempt,
-		},
+		Metadata:  metadata,
 		// TODO: We write annotations as labels on the docker containers. All
 		// these annotations will be read back as labels. Need to fix this.
 		// Also filter out labels only relevant to this shim.
@@ -184,18 +180,22 @@ func (ds *dockerService) ListPodSandbox(filter *runtimeApi.PodSandboxFilter) ([]
 	result := []*runtimeApi.PodSandbox{}
 	for i := range containers {
 		c := containers[i]
-		if len(filter.GetName()) > 0 {
-			sandboxName, _, _, _, err := parseSandboxName(c.Names[0])
-			if err != nil || sandboxName != filter.GetName() {
-				continue
-			}
-		}
 
-		s := toRuntimeAPISandbox(&c)
-		if filterOutReadySandboxes && s.GetState() == runtimeApi.PodSandBoxState_READY {
+		converted, err := toRuntimeAPISandbox(&c)
+		if err != nil {
+			glog.V(5).Infof("Unable to convert docker to runtime API sandbox: %v", err)
 			continue
 		}
-		result = append(result, s)
+		if len(filter.GetName()) > 0 && converted.Metadata.GetName() != filter.GetName() {
+			// TODO: Remove "name" from the SandboxFilter because name can no
+			// longer be used to identify a container.
+			continue
+		}
+		if filterOutReadySandboxes && converted.GetState() == runtimeApi.PodSandBoxState_READY {
+			continue
+		}
+
+		result = append(result, converted)
 	}
 	return result, nil
 }
@@ -208,7 +208,7 @@ func makeSandboxDockerConfig(c *runtimeApi.PodSandboxConfig, image string) *dock
 
 	hc := &dockercontainer.HostConfig{}
 	createConfig := &dockertypes.ContainerCreateConfig{
-		Name: buildSandboxName(c),
+		Name: makeSandboxName(c),
 		Config: &dockercontainer.Config{
 			Hostname: c.GetHostname(),
 			// TODO: Handle environment variables.