d3fdc140a851d4c74c5251bb86368f0e9f259207
evol262 <87092321+evol262@users.noreply.github.com>
2021-03-31 18:51:37 -0400

    Bump the version

--- packaging/common.mk
+++ packaging/common.mk
@@ -1,10 +1,9 @@
 ARCH=$(shell uname -m)
 BUILDTIME=$(shell date -u -d "@$${SOURCE_DATE_EPOCH:-$$(date +%s)}" --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
-DEFAULT_PRODUCT_LICENSE:=Community Engine
 GO_VERSION:=1.13.11
 PLATFORM=Docker Engine - Community
 SHELL:=/bin/bash
-VERSION?=0.0.0-dev
+VERSION?=0.1.0
 
 export BUILDTIME
 export PLATFORM
