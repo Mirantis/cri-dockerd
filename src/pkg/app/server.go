278a1fc2e34192b6818a589ae7df98911396b0f3
Ryan Barry <rbarry@mirantis.com>
2021-09-29 00:55:03 -0400

    Sanitize the source layout so it's more natural to work with

---
+++ src/pkg/app/server.go
@@ -0,0 +1,168 @@
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+	"github.com/Mirantis/cri-dockerd/core"
+	"github.com/Mirantis/cri-dockerd/pkg/app/options"
+	dockerremote "github.com/Mirantis/cri-dockerd/remote"
+	"github.com/Mirantis/cri-dockerd/version"
+	"k8s.io/component-base/cli/flag"
+	utilflag "k8s.io/component-base/cli/flag"
+	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+	"k8s.io/klog"
+	"k8s.io/kubernetes/pkg/kubelet/cri/streaming"
+	"net/url"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+const (
+	componentDockerCRI = "cri-dockerd"
+)
+
+// NewDockerCRICommand creates a *cobra.Command object with default parameters
+func NewDockerCRICommand(stopCh <-chan struct{}) *cobra.Command {
+	cleanFlagSet := pflag.NewFlagSet(componentDockerCRI, pflag.ContinueOnError)
+	cleanFlagSet.SetNormalizeFunc(flag.WordSepNormalizeFunc)
+	kubeletFlags := options.NewDockerCRIFlags()
+
+	cmd := &cobra.Command{
+		Use:  componentDockerCRI,
+		Long: `CRI that connects to Docker Daemon`,
+		// The cri-dockerd has special flag parsing requirements to enforce flag precedence rules,
+		// so we do all our parsing manually in Run, below.
+		// DisableFlagParsing=true provides the full set of flags passed to the cri-dockerd in the
+		// `args` arg to Run, without Cobra's interference.
+		DisableFlagParsing: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			// initial flag parse, since we disable cobra's flag parsing
+			if err := cleanFlagSet.Parse(args); err != nil {
+				cmd.Usage()
+				klog.Fatal(err)
+			}
+
+			// check if there are non-flag arguments in the command line
+			cmds := cleanFlagSet.Args()
+			if len(cmds) > 0 {
+				cmd.Usage()
+				klog.Fatalf("unknown command: %s", cmds[0])
+			}
+
+			// short-circuit on help
+			help, err := cleanFlagSet.GetBool("help")
+			if err != nil {
+				klog.Fatal(`"help" flag is non-bool, programmer error, please correct`)
+			}
+			if help {
+				cmd.Help()
+				return
+			}
+
+			verflag, _ := cleanFlagSet.GetBool("version")
+			if verflag {
+				fmt.Fprintf(cmd.OutOrStderr(), "%s %s\n", version.PlatformName, version.FullVersion())
+				return
+			}
+
+			// short-circuit on verflag
+			utilflag.PrintFlags(cleanFlagSet)
+
+			if err := RunCriDockerd(kubeletFlags, stopCh); err != nil {
+				klog.Fatal(err)
+			}
+		},
+	}
+
+	// keep cleanFlagSet separate, so Cobra doesn't pollute it with the global flags
+	kubeletFlags.AddFlags(cleanFlagSet)
+	options.AddGlobalFlags(cleanFlagSet)
+	cleanFlagSet.BoolP("help", "h", false, fmt.Sprintf("help for %s", cmd.Name()))
+	cleanFlagSet.Bool("version", false, "prints the version of cri-dockerd")
+
+	// ugly, but necessary, because Cobra's default UsageFunc and HelpFunc pollute the flagset with global flags
+	const usageFmt = "Usage:\n  %s\n\nFlags:\n%s"
+	cmd.SetUsageFunc(func(cmd *cobra.Command) error {
+		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine(), cleanFlagSet.FlagUsagesWrapped(2))
+		return nil
+	})
+	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine(), cleanFlagSet.FlagUsagesWrapped(2))
+	})
+	return cmd
+}
+
+// RunCriDockerd starts cri-dockerd
+func RunCriDockerd(f *options.DockerCRIFlags, stopCh <-chan struct{}) error {
+	r := &f.ContainerRuntimeOptions
+
+	// Initialize docker client configuration.
+	dockerClientConfig := &core.ClientConfig{
+		DockerEndpoint:            r.DockerEndpoint,
+		RuntimeRequestTimeout:     r.RuntimeRequestTimeout.Duration,
+		ImagePullProgressDeadline: r.ImagePullProgressDeadline.Duration,
+	}
+
+	// Initialize network plugin settings.
+	pluginSettings := core.NetworkPluginSettings{
+		HairpinMode:        "none",
+		PluginName:         f.NetworkPluginName,
+		PluginConfDir:      f.CNIConfDir,
+		PluginBinDirString: f.CNIBinDir,
+		PluginCacheDir:     f.CNICacheDir,
+		MTU:                int(f.NetworkPluginMTU),
+		NonMasqueradeCIDR:  f.NonMasqueradeCIDR,
+	}
+
+	// Initialize streaming configuration. (Not using TLS now)
+	streamingConfig := &streaming.Config{
+		// Use a relative redirect (no scheme or host).
+		BaseURL:                         &url.URL{Path: "/cri/"},
+		StreamIdleTimeout:               r.StreamingConnectionIdleTimeout.Duration,
+		StreamCreationTimeout:           streaming.DefaultConfig.StreamCreationTimeout,
+		SupportedRemoteCommandProtocols: streaming.DefaultConfig.SupportedRemoteCommandProtocols,
+		SupportedPortForwardProtocols:   streaming.DefaultConfig.SupportedPortForwardProtocols,
+	}
+
+	// Standalone cri-dockerd will always start the local streaming server.
+	ds, err := core.NewDockerService(dockerClientConfig, r.PodSandboxImage, streamingConfig, &pluginSettings,
+		f.RuntimeCgroups, r.CgroupDriver, r.CriDockerdRootDirectory)
+	if err != nil {
+		return err
+	}
+
+	if _, err := ds.UpdateRuntimeConfig(nil, &runtimeapi.UpdateRuntimeConfigRequest{
+		RuntimeConfig: &runtimeapi.RuntimeConfig{
+			NetworkConfig: &runtimeapi.NetworkConfig{
+				PodCidr: f.PodCIDR,
+			},
+		},
+	}); err != nil {
+		return err
+	}
+
+	klog.V(2).Infof("Starting the GRPC server for the docker CRI shim.")
+	server := dockerremote.NewDockerServer(f.RemoteRuntimeEndpoint, ds)
+	if err := server.Start(); err != nil {
+		return err
+	}
+
+	<-stopCh
+	return nil
+}
---
+++ src/remote/docker_server.go
@@ -0,0 +1,96 @@
+// +build !dockerless
+
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package remote
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"google.golang.org/grpc"
+	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+	"k8s.io/klog/v2"
+	"github.com/Mirantis/cri-dockerd/core"
+	"k8s.io/kubernetes/pkg/kubelet/util"
+)
+
+// maxMsgSize use 16MB as the default message size limit.
+// grpc library default is 4MB
+const maxMsgSize = 1024 * 1024 * 16
+
+// DockerServer is the grpc server of cri-dockerd.
+type DockerServer struct {
+	// endpoint is the endpoint to serve on.
+	endpoint string
+	// service is the docker service which implements runtime and image services.
+	service core.CRIService
+	// server is the grpc server.
+	server *grpc.Server
+}
+
+// NewDockerServer creates the cri-dockerd grpc server.
+func NewDockerServer(endpoint string, s core.CRIService) *DockerServer {
+	return &DockerServer{
+		endpoint: endpoint,
+		service:  s,
+	}
+}
+
+func getListener(addr string) (net.Listener, error) {
+	addrSlice := strings.SplitN(addr, "://", 2)
+	proto := addrSlice[0]
+	listenAddr := addrSlice[1]
+	switch proto {
+	case "fd":
+		return listenFD(listenAddr)
+	default:
+		return util.CreateListener(addr)
+	}
+}
+
+// Start starts the crid-ckerd grpc server.
+func (s *DockerServer) Start() error {
+	// Start the internal service.
+	if err := s.service.Start(); err != nil {
+		klog.ErrorS(err, "Unable to start cri-dockerd service")
+		return err
+	}
+
+	klog.V(2).InfoS("Start cri-dockerd grpc server")
+	l, err := getListener(s.endpoint)
+	if err != nil {
+		return fmt.Errorf("cri-dockerd failed to listen on %q: %v", s.endpoint, err)
+	}
+	// Create the grpc server and register runtime and image services.
+	s.server = grpc.NewServer(
+		grpc.MaxRecvMsgSize(maxMsgSize),
+		grpc.MaxSendMsgSize(maxMsgSize),
+	)
+	runtimeapi.RegisterRuntimeServiceServer(s.server, s.service)
+	runtimeapi.RegisterImageServiceServer(s.server, s.service)
+	go func() {
+		if err := s.server.Serve(l); err != nil {
+			klog.ErrorS(err, "Failed to serve connections from cri-dockerd")
+			os.Exit(1)
+		}
+	}()
+	handleNotify()
+	return nil
+}
