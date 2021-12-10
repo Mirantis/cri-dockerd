103e83cc08ee7d3804ecedf2b4ecc9531b5d6a48
Ryan Barry <rbarry@mirantis.com>
2021-12-09 23:55:14 -0500

    Packaging updates for release

--- packaging/common.mk
+++ packaging/common.mk
@@ -1,9 +1,9 @@
 ARCH=$(shell uname -m)
 BUILDTIME=$(shell date -u -d "@$${SOURCE_DATE_EPOCH:-$$(date +%s)}" --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
-GO_VERSION:=1.13.11
+GO_VERSION:=1.16.8
 PLATFORM=cri-dockerd
 SHELL:=/bin/bash
-VERSION?=0.1.0-dev
+VERSION?=0.2.0-dev
 
 export BUILDTIME
 export PLATFORM
