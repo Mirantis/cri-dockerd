23513f4cfe8f02bc02130b4edba2c9d2c8fd320b
Ryan Barry <rbarry@mirantis.com>
2023-05-11 17:39:42 -0400

    Release 0.3.2

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version of the product
-	Version = "0.3.1"
+	Version = "0.3.2"
 	// PreRelease is set during the build
 	PreRelease = ""
 	// GitCommit is set during the build
