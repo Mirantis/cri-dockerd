667f45c10fedbf36cc56077d2527169865ced887
Nick Neisen <nneisen@mirantis.com>
2023-10-27 12:17:37 -0600

    Release 0.3.7

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version of the product
-	Version = "0.3.6"
+	Version = "0.3.7"
 	// PreRelease is set during the build
 	PreRelease = ""
 	// GitCommit is set during the build
