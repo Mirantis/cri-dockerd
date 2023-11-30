34fa858934b625f61960499e8296fb0f7b3aa217
Nick Neisen <nneisen@mirantis.com>
2023-11-30 12:20:22 -0700

    Code changes for new packages

--- network/hostport/hostport_manager.go
+++ network/hostport/hostport_manager.go
@@ -31,7 +31,6 @@ import (
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
-	iptablesproxy "k8s.io/kubernetes/pkg/proxy/iptables"
 	"k8s.io/kubernetes/pkg/util/conntrack"
 	utiliptables "k8s.io/kubernetes/pkg/util/iptables"
 	"k8s.io/utils/exec"
@@ -156,7 +155,7 @@ func (hm *hostportManager) Add(
 		writeLine(natRules, "-A", string(chain),
 			"-m", "comment", "--comment", fmt.Sprintf(`"%s hostport %d"`, podFullName, pm.HostPort),
 			"-s", podIP,
-			"-j", string(iptablesproxy.KubeMarkMasqChain))
+			"-j", "KUBE-MARK-MASQ") // iptablesproxy "k8s.io/kubernetes/pkg/proxy/iptables" is too cool to stay imported
 
 		// DNAT to the podIP:containerPort
 		hostPortBinding := net.JoinHostPort(podIP, strconv.Itoa(int(pm.ContainerPort)))
@@ -424,7 +423,7 @@ func getExistingHostportIPTablesRules(
 	if err != nil { // if we failed to get any rules
 		return nil, nil, fmt.Errorf("failed to execute iptables-save: %v", err)
 	}
-	existingNATChains := utiliptables.GetChainLines(utiliptables.TableNAT, iptablesData.Bytes())
+	existingNATChains := utiliptables.GetChainsFromTable(iptablesData.Bytes())
 
 	existingHostportChains := make(map[utiliptables.Chain]string)
 	existingHostportRules := []string{}
@@ -432,7 +431,10 @@ func getExistingHostportIPTablesRules(
 	for chain := range existingNATChains {
 		if strings.HasPrefix(string(chain), string(kubeHostportsChain)) ||
 			strings.HasPrefix(string(chain), kubeHostportChainPrefix) {
-			existingHostportChains[chain] = string(existingNATChains[chain])
+			if _, ok := existingNATChains[chain]; ok {
+				// This just means true
+				existingHostportChains[chain] = ""
+			}
 		}
 	}
 
