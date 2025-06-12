43a8c9f60b66fb0f05ee4ff7c3b0840541af45c4
Derek Nola <derek.nola@suse.com>
2025-06-12 14:56:10 -0700

    [release/0.3] Migrate from docker/distribution/reference to distribution/reference
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/containernetworking/cni v1.1.2
 	github.com/coreos/go-systemd/v22 v22.5.0
 	github.com/davecgh/go-spew v1.1.1
-	github.com/docker/distribution v2.8.1+incompatible
+	github.com/distribution/reference v0.6.0
 	github.com/docker/docker v20.10.17+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/emicklei/go-restful v2.16.0+incompatible
@@ -46,6 +46,7 @@ require (
 	github.com/containerd/cgroups v1.0.1 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.3 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
--- libdocker/helpers.go
+++ libdocker/helpers.go
@@ -18,15 +18,16 @@ package libdocker
 
 import (
 	"fmt"
-	"github.com/docker/go-connections/nat"
-	v1 "k8s.io/cri-api/pkg/apis/runtime/v1"
-	"k8s.io/kubernetes/pkg/apis/core"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
-	dockerref "github.com/docker/distribution/reference"
+	"github.com/docker/go-connections/nat"
+	v1 "k8s.io/cri-api/pkg/apis/runtime/v1"
+	"k8s.io/kubernetes/pkg/apis/core"
+
+	dockerref "github.com/distribution/reference"
 	dockertypes "github.com/docker/docker/api/types"
 	godigest "github.com/opencontainers/go-digest"
 	"github.com/sirupsen/logrus"
@@ -47,7 +48,7 @@ func ParseDockerTimestamp(s string) (time.Time, error) {
 // (`foo@sha256:xyz`).
 func matchImageTagOrSHA(inspected dockertypes.ImageInspect, image string) bool {
 	// The image string follows the grammar specified here
-	// https://github.com/docker/distribution/blob/master/reference/reference.go#L4
+	// https://github.com/distribution/reference/blob/master/reference.go#L4
 	named, err := dockerref.ParseNormalizedNamed(image)
 	if err != nil {
 		logrus.Errorf("Couldn't parse image (%s) reference: %v", image, err)
